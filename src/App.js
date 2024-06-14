9aacd076846715978b19495bc04c3ef67503fec0
常全辉 <zhi28881407@163.com>
2024-06-14 17:15:42 +0800

    subscribe use useSyncExternalStore

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
+import { create } from "./c-zustand/index"
+import { useEffect } from 'react';
+
+
+const useSelfStore = create((set) => {
+  return {
+    a: "1",
+    b: "2",
+    c: "3",
+    updateA: (v) => set({ a: v }),
+    updateB: (v) => set({ b: v }),
+
+  }
+})
+
+function App2() {
+  const updateA = useSelfStore(state => state.updateA)
+  const updateB = useSelfStore(state => state.updateB)
+
+  const a = useSelfStore(state => state.a)
+  const b = useSelfStore(state => state.b)
+  const c = useSelfStore(state => state.c)
+  return <>
+    <p>
+      content: {a} - {b} - {c}
+    </p>
+    <button onClick={() => updateA("996")}>
+      testA
+    </button>
+    <button onClick={() => updateB("007")}>
+      testB
+    </button>
+
+    <button onClick={() => {
+      updateA("1")
+      updateB("2")
+    }}>
+      reset
+    </button>
+  </>
+}
+
+function App3() {
+  const c = useSelfStore(state => state.c)
+
+  useEffect(() => {
+    console.log("app3 update")
+  })
+  return <>
+    <div>app3 -{c}</div>
+  </>
+}
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <App2 />
+      <App3 />
+
     </div>
   );
 }
--- src/c-zustand/index.js
+++ src/c-zustand/index.js
@@ -1,35 +1,42 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect,useSyncExternalStore } from "react"
 
 const createStore = (createState) => {
+    /* 当前最新的state 数据 */
     let state;
 
+    /* 回调函数的存储 */
     const listeners = new Set();
 
     const setState = (partial, isReplace) => {
+        /* 如果 这个 set 是个function的话就拿到对应的返回值*/
         const nextState = typeof partial === "function" ? partial(state) : partial
-
+        /* 判断两个对象是否相等 相等就不会去更新 */
         if (!Object.is(nextState, state)) {
             const previousState = state;
-
+            /* 是否是将数据直接进行替换 */
             if (!isReplace) {
                 state = (typeof nextState !== "object" || nextState === null) ? nextState : { ...state, ...nextState }
             } else {
                 state = nextState
             }
-
+            /*
+             state 更新后 依次去触发对应的监听函数 
+             比如a 组件用到了 这个state 就会去触发a组件state 的gengxin
+             */
             listeners.forEach(listener => listener(state, previousState))
         }
     }
-
+    /* 获取最新的state */
     const getState = () => {
         return state
     }
 
+    /* 和当前的store进行监听 */
     const subscribe = (listener) => {
         listeners.add(listener)
         return () => listeners.delete(listener)
     }
-
+    /* 销毁 */
     const destroy = () => {
         listeners.clear()
     }
@@ -40,32 +47,28 @@ const createStore = (createState) => {
         subscribe,
         destroy
     }
-
+    /* 获取state */
     state = createState(setState, getState, api)
 
     return api
 
 }
 
-
-function useStore(api, selector) {
-
-    const [, forceRender] = useState([])
-
-    useEffect(() => {
-        api.subscribe((state, prevState) => {
-            if (selector(state) !== selector(prevState)) {
-                forceRender([])
-            }
-        })
-    }, [])
-    return selector(api.getState())
+/* 与对应的函数建立起来监听关系 */
+function useStoreUseSyncExternalStore(api, selector){
+    return useSyncExternalStore(api.subscribe, ()=>selector(api.getState()))
 }
 
-
+/* 创建函数 */
 export const create = (createState) => {
+    /* 先创建store 生成 api */
     const api = createStore(createState)
-    const useBoundStore = (selector) => useStore(api, selector)
+    /* 然后用 包装一个函数  接受一个selector 去主动的选择数据 */
+    const useBoundStore = (selector) => useStoreUseSyncExternalStore(api, selector)
+    /* 
+        将操作方法绑定到这个函数上面
+        
+     */
     Object.assign(useBoundStore, api)
     return useBoundStore
 }
